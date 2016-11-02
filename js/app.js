79c0ea5dd518e70261371e067a4d71163c3a5c99
orjellingsen <orjanell@gmail.com>
2016-11-03 00:41:59 +0100

    Restructured js to View/ViewModel

--- js/app.js
+++ js/app.js
@@ -1,46 +1,60 @@
-var map;
-var infowindow;
-var placeList = ko.observableArray();
+function AppViewModel() {
+  var map;
+  var infowindow;
+  this.placeList = ko.observableArray();
+  this.query = ko.observable();
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  infowindow = new google.maps.InfoWindow({
-    pixelOffset: new google.maps.Size(0, -40)
-  });
+  // Initialize the map
+  this.initMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  var nearbyPlaces = new google.maps.places.PlacesService(map);
-  nearbyPlaces.nearbySearch(nearbySearchOptions, createMarkers);
+    infowindow = new google.maps.InfoWindow({
+      pixelOffset: new google.maps.Size(0, -40)
+    });
+    createPlaceList();
+  }
 
-  var searchBox = new google.maps.places.SearchBox(document.getElementById('places-search'));
-  searchBox.setBounds(map.getBounds());
-}
+  // Create a places list
+  this.createPlaceList = function() {
+    var nearbyPlaces = new google.maps.places.PlacesService(map);
+    nearbyPlaces.nearbySearch({
+      location: mapCenter,
+      radius: 2500,
+      type: ['restaurant']
+    }, createMarkers);
+  }
 
-function createMarkers(results, status) {
-  if(status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var marker = addMarker(results[i]);
-      console.log(results[i]);
-      placeList.push(results[i]);
+  // Create a list of markers based on results from nearbySearch
+  this.createMarkers = function(results, status) {
+    if(status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var marker = addMarker(results[i]);
+        //console.log(results[i]);
+        placeList.push(results[i]);
+      }
+    } else {
+      console.log('Error: PlacesService not OK');
     }
-  } else {
-    console.log('Error: PlacesService not OK');
+  }
+
+  // Add a marker to the map and listen for clicks
+  this.addMarker = function(place) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      openInfoWindow(place);
+    });
+  }
+
+  // Open infowindow
+  this.openInfoWindow = function(place) {
+    infowindow.setContent(place.name);
+    infowindow.setPosition(place.geometry.location);
+    infowindow.open(map, addMarker(place));
   }
 }
 
-function addMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    openInfoWindow(place);
-  });
-}
-
-function openInfoWindow(place) {
-  infowindow.setContent(place.name);
-  infowindow.setPosition(place.geometry.location);
-  infowindow.open(map, addMarker(place));
-}
-
-ko.applyBindings(placeList);
+// Apply knockout bindings
+ko.applyBindings(AppViewModel);
