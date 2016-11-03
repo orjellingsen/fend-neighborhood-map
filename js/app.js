7270815c5ab08392fb6a47ee5db846a6bacb6406
orjellingsen <orjanell@gmail.com>
2016-11-04 00:58:32 +0100

    Remade several functions. Markers are now removed when a new search is entered. Simplified infowindow open. TODO: Find a way to make new search go faster

--- js/app.js
+++ js/app.js
@@ -1,73 +1,78 @@
 function AppViewModel() {
   var map;
   var infowindow;
-  var markers = [];
-  this.placeList = ko.observableArray();
-  this.placesFilter = ko.observable();
+  this.markers = ko.observableArray([]);
+  this.placesFilter = ko.observable('');
 
   // Initialize the map
   this.initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    infowindow = new google.maps.InfoWindow({
-      pixelOffset: new google.maps.Size(0, -40)
-    });
-    createPlaceList();
+    infowindow = new google.maps.InfoWindow();
+    getPlaces();
   }
 
-  this.placesFilter.subscribe(function(value) {
-    removeMarkers();
-    placeList([]);
-    createPlaceList();
-  });
-
-  this.createPlaceList = function() {
-    console.log('createPlaceList function called');
+  // Get a list of places
+  this.getPlaces = function() {
     var nearbyPlaces = new google.maps.places.PlacesService(map);
     nearbyPlaces.nearbySearch({
       location: mapCenter,
-      radius: 2500,
-      type: ['restaurant'],
+      radius: 2000,
+      types: ['restaurant', 'bar', 'cafe'],
       keyword: this.placesFilter()
     }, createMarkers);
   }
 
   // Create a list of markers based on results from nearbySearch
   this.createMarkers = function(results, status) {
-    //TODO remake this function to push marks into array, and find a way to have KO still display marker list
     if(status === google.maps.places.PlacesServiceStatus.OK) {
+      var bounds = new google.maps.LatLngBounds();
       for (var i = 0; i < results.length; i++) {
         var marker = addMarker(results[i]);
-        //console.log(results[i]);
-        placeList.push(results[i]);
+      /*  bounds.extend(new google.maps.LatLng(
+          results[i].geometry.location.lat(),
+          results[i].geometry.location.lng()));*/
       }
     } else {
       console.log('Error: PlacesService not OK');
     }
   }
 
-  this.removeMarkers = function() {
-    createMarkers(null);
-    //TODO link this function to new createmarkers
-  }
-
   // Add a marker to the map and listen for clicks
   this.addMarker = function(place) {
     var marker = new google.maps.Marker({
       map: map,
-      position: place.geometry.location
+      title: place.name,
+      position: place.geometry.location,
+      place_id: place.place_id,
+      animation: google.maps.Animation.DROP
     });
     google.maps.event.addListener(marker, 'click', function() {
-      openInfoWindow(place);
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
     });
+    markers.push(marker);
+    return marker;
+  }
+
+  // Remove all markers from the marker array
+  this.removeMarkers = function() {
+    console.log(markers().length);
+    for (var i = 0; i < markers().length; i++ ) {
+     markers()[i].setMap(null);
+    }
+    markers([]);
   }
 
   // Open infowindow
   this.openInfoWindow = function(place) {
-    infowindow.setContent(place.name);
-    infowindow.setPosition(place.geometry.location);
-    infowindow.open(map, addMarker(place));
+    infowindow.setContent(place.title);
+    infowindow.open(map, this);
   }
+
+  this.placesFilter.subscribe(function(value) {
+    removeMarkers();
+    getPlaces();
+  });
 }
 
 // Apply knockout bindings
